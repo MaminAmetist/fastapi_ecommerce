d5df14a608ac83fe7e7d10ae259f42915c16f16b
Anna <anna.nezdulkina@mail.ru>
2025-12-18 16:14:37 +0700

    categories.py transition to async engine

--- app/database.py
+++ app/database.py
@@ -1,12 +1,28 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
 
 DATABASE_URL = "sqlite:///ecommerce.db"
-# DATABASE_URL = "postgresql+psycopg2://username:password@localhost:5432/dbname"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(bind=engine)
+
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
